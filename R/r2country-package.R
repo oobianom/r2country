744832e0e02026a56a545e11573dccb033b784bf
Obi Obianom <oobianom@umaryland.edu>
2023-08-31 01:49:33 -0400

    Preparation of to-dos

---
+++ R/r2country-package.R
@@ -0,0 +1,25 @@
+#' @keywords internal
+"_PACKAGE"
+
+.onLoad <- function(libname, pkgname) {
+  # hooks for future enhancements
+}
+
+
+.onAttach <- function(libname, pkgname) {
+  # hooks for future enhancements
+}
+
+
+# data fetch
+### time in the country
+### currency change in the country
+### population of the country
+### continent of the country
+### presidents of the country
+### vicepresent of the country
+### states in the country
+### calling code
+### official languages
+###
+
---
+++ R/states.R
@@ -0,0 +1,25 @@
+names(countries)=countries
+#' Get country states
+#'
+#' Fetch all country states
+#'
+#' @rdname countrystates
+#' @format NULL
+#' @docType NULL
+#' @keywords NULL
+#' @export
+states<-lapply(countries,function(i){
+  rlang::new_function(
+    args = rlang::exprs(... = , .noWS = NULL, .renderHook = NULL),
+    rlang::expr({
+      ggg = !!i
+      print(ggg)
+      print("heelo")
+    }),
+    env = asNamespace("quickcode")
+  )
+})
+
+
+
+
