5472b2899439fa418f63961f260c4adedeb2e809
Obi Obianom <oobianom@umaryland.edu>
2023-09-03 01:57:52 -0400

    Update data.R

--- R/data.R
+++ R/data.R
@@ -101,3 +101,37 @@
 
 "country_population"
 
+
+
+#' Data for Country Money
+#'
+#' Data for all country currency names and symbols
+#'
+#' @format A data frame with all countries and columns for currency
+#' \describe{
+#'    \item{x}{country name}
+#'    }
+#' @source {Internal data for currencies belonging to every country}
+#'
+#' @examples
+#' data(country_population)
+#'
+
+"country_continent"
+
+
+#' Data for Country Money
+#'
+#' Data for all country currency names and symbols
+#'
+#' @format A data frame with all countries and columns for currency
+#' \describe{
+#'    \item{x}{country name}
+#'    }
+#' @source {Internal data for currencies belonging to every country}
+#'
+#' @examples
+#' data(country_population)
+#'
+
+"country_capital"
