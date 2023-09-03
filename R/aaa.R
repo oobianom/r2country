bdef4bf12677b20a1c721e7f3d13e102c8220d63
Obi Obianom <oobianom@umaryland.edu>
2023-09-03 01:56:36 -0400

    Update aaa.R

--- R/aaa.R
+++ R/aaa.R
@@ -5,7 +5,8 @@ data(city_time) # time by city
 data(country_names) # country names
 data(country_money) # countries currency
 data(country_language) # country language
-# continent countries
+data(country_capital) #country capitals
+data(country_continent)# continent countries
 
 # merge country data
 data.pck.cntry <- cbind(
@@ -13,6 +14,8 @@ data.pck.cntry <- cbind(
   country_money[-1],
   country_language[-1],
   country_population[-1],
-  country_calling_code[-1]
+  country_calling_code[-1],
+  country_continent[-1],
+  country_capital[-1]
 )
 
