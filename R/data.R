9364b39ce5493c8a881c671cf4d94ac92745a287
Obi Obianom <oobianom@umaryland.edu>
2023-09-04 03:37:29 -0400

    Update man page

--- R/data.R
+++ R/data.R
@@ -1,7 +1,9 @@
-#' Data for Country Money
+#' Dataset for Country Currencies
 #'
 #' Data for all country currency names and symbols
 #'
+#' @docType data
+#' @usage data(country_money)
 #' @format A data frame with all countries and columns for currency
 #' \describe{
 #'    \item{ID}{Identifier for countries}
@@ -13,11 +15,12 @@
 #' @source {Internal data for currencies belonging to every country}
 #' @return data for currencies by country
 #' @references \url{wikipedia.com}
+#' @seealso \code{\link{country_names}} for linkage of IDs with country names
 #' @keywords country currencies, currency by country
 #' @examples
 #' data(country_money)
 #'
-#' #view content of the countries currency
+#' # view content of the countries currency
 #' head(country_money)
 #'
 "country_money"
@@ -28,6 +31,8 @@
 #'
 #' Data for all country currency names and associated ID
 #'
+#' @docType data
+#' @usage data(country_names)
 #' @format A data frame with the names of all countries
 #' \describe{
 #'    \item{ID}{Identifier for the countries}
@@ -36,12 +41,12 @@
 #' @source {Internal data consisting of all country names}
 #' @return data for names of country
 #' @references \url{wikipedia.com}
-#' @keywords country names and ID for relational datasets
+#' @keywords country names
 #' @examples
 #' # load the country names
 #' data(country_names)
 #'
-#' #view content of the countries currency
+#' # view content of the countries currency
 #' head(country_names)
 #'
 
@@ -49,35 +54,54 @@
 
 
 
-#' Data for Country Money
+#' Dataset of country official languages
 #'
-#' Data for all country currency names and symbols
+#' Data for all countries and their associated languages
 #'
-#' @format A data frame with all countries and columns for currency
+#' @docType data
+#' @usage data(country_language)
+#' @format A data frame with the languages of countries
 #' \describe{
-#'    \item{x}{country name}
+#'    \item{ID}{Identifier for the countries}
+#'    \item{officiallanguage}{official languages of countries}
 #'    }
-#' @source {Internal data for currencies belonging to every country}
-#'
+#' @source {Internal data consisting of all country IDs and their official languages}
+#' @return country ID and official languages
+#' @references \url{wikipedia.com}
+#' @keywords country languages, language of countries
+#' @seealso \code{\link{country_names}} for linkage of IDs with country names
 #' @examples
-#' data(country_money)
+#' # load the country ID and languages
+#' data(country_language)
+#'
+#' # view content of the countries languages
+#' head(country_language)
 #'
 
 "country_language"
 
 
-#' Data for Country Money
+#' Dataset of current time in Cities
 #'
-#' Data for all country currency names and symbols
+#' Dataset containing time different in various cities with respect to USA/New York time
 #'
-#' @format A data frame with all countries and columns for currency
+#' @docType data
+#' @usage data(city_time)
+#' @format A data frame with the languages of countries
 #' \describe{
-#'    \item{x}{country name}
+#'    \item{City}{Names of cities}
+#'    \item{Timediff}{Time difference relative to New York time}
 #'    }
-#' @source {Internal data for currencies belonging to every country}
-#'
+#' @source {Internal data consisting of cities and their corresponding time}
+#' @return cities and the current time relative to New York
+#' @references \url{timeanddate.com}
+#' @keywords city time, current time in place, current time country
 #' @examples
-#' data(country_money)
+#' # load the cities and languages dataset
+#' data(city_time)
+#'
+#' # view content of the city-language data
+#' head(city_time)
 #'
 
 "city_time"
